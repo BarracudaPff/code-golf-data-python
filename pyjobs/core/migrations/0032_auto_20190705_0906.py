2f756965729d89d97f21c1401d0514a25ca8b771
Iaroslav Sokolov <sokolov.yas@gmail.com>
2020-05-23 17:20:55 +0300

    Fix folder bug. Delete _auto_ files

--- pyjobs/core/migrations/0032_auto_20190705_0906.py
+++
@@ -1,9 +0,0 @@
-def generated_premium_dates(apps, schema_editor):
-	Job = apps.get_model("core", "Job")
-	for job in Job.objects.all():
-		if job.premium == True:
-			job.premium_at = job.created_at
-		job.save()
-class Migration(migrations.Migration):
-	dependencies = [("core", "0031_job_premium_at")]
-	operations = [migrations.RunPython(generated_premium_dates)]
